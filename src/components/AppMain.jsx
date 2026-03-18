45f44ad057724a62d14d3a81702b821aac897d06
stefanosorrenti <stefanosorrenti01@gmail.com>
2026-03-18 15:28:31 +0100

    AppMain: add reactive inputs and centralize their logic

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -1,15 +1,47 @@
 //IMPORTS
 import { useState } from "react"
 import { useEffect } from "react"
-
 export default function AppMain() {
 
     //USE STATE
+    const [formData, setFormData] = useState({  //Creo un oggetto di stato che conterrà delle proprietà con lo stesso nome del valore dell'attributo 'name'
+
+        author: '', 
+
+        title : '',
+
+        content: '',
+
+        content: '',
+
+        visibility: false
+
+    })
+
+    
+    
+    
     
     //FUCNTIONS
-    function handlerSubmit(e) { //Funzione che si attiva al submit del form
+    function handlerSubmit(e) { //Funzione che attiverò al submit del form
         e.preventDefault() //Blocco il comportamento naturale del form
 
+    }
+
+    function handlerValue (e) { //Funzione che attiverò qunado ci saranno dei cambiamenti nei vari input 
+
+        //Creo una costante in cui dico 'Se l'attributo type dell'elemento che ha scatenato l'evento (input) è uguale a 'checkbox' il valore di questa ' variabile è 
+        //uguale all'attributo checked dell'elemento che ha scatenato l'evento, altrimenti è uguale all'ttributo value dell'elmeneto che ha scatenato l'evento.
+        //Cosi facendo se il mio elemento è un checkbox posso settare ricavarmi il suo valroe booleano
+        const type = e.target.type === 'checkbox' ? e.target.checked : e.target.value
+
+        //Setto la mia variabile di statao cosi 'Crea un nuovo oggetto e inserisci tutti i dati clonati da l'oggetto form data'
+        
+        setFormData({...formData, [e.target.name] : type})  //e aggiungi una nuova proprietà, il nome della proprietà e [IL VALORE]  l'attribbuto name dell'elemento 
+        // che scatenato l'evento  e il valore della proprietà è la variabile dichiarata prima
+        console.log(formData);
+        
+        
     }
 
     return (
@@ -25,16 +57,16 @@ export default function AppMain() {
                 <form onSubmit={handlerSubmit}>
 
                     {/* Post authour */}
-                    <input name="author" placeholder="Autore" type="text" />
+                    <input name="author" placeholder="Autore" type="text" onChange={handlerValue} value={formData.author} />
 
                     {/* Post title */}
-                    <input name="title" placeholder="Titolo" type="text" />
+                    <input name="title" placeholder="Titolo" type="text" onChange={handlerValue} value={formData.title} />
 
                     {/* Post content */}
-                    <textarea name="content" id=""></textarea>
+                    <textarea name="content"  onChange={handlerValue} value={formData.content}></textarea>
 
                     {/* Set post access */}
-                    <input name="checkbox" type="checkbox" />
+                    <input name="visibility" type="checkbox"  onChange={handlerValue} checked={formData.visibility} />
                     
                     {/* Send post */}
                     <button>invia post</button>
