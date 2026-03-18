11aa61b8934214fe00ca6672a6e8da1655388993
stefanosorrenti <stefanosorrenti01@gmail.com>
2026-03-18 14:10:12 +0100

    add html form structure

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -1,9 +1,34 @@
-export default function AppMain () {
+export default function AppMain() {
 
     return (
         /* Main */
         <main>
-            PROVA MAIN
+
+            {/* FORM SECTION */}
+
+            <section>
+                <h2>Inserisci il tuo post.</h2>
+
+                {/* Form */}
+                <form>
+
+                    {/* Post authour */}
+                    <input placeholder="Autore" type="text" />
+
+                    {/* Post title */}
+                    <input placeholder="Titolo" type="text" />
+
+                    {/* Post content */}
+                    <textarea name="" id=""></textarea>
+
+                    {/* Set post access */}
+                    <input type="checkbox" />
+                    
+                    {/* Send post */}
+                    <button>invia post</button>
+                </form>
+    
+            </section>
         </main>
     )
 }
